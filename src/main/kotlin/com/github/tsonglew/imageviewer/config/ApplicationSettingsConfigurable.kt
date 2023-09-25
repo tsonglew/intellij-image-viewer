bcb68dede7d3dfdffa110d2373a56f683ecec1f9
Tsonglew <tsonglew@gmail.com>
2023-09-25 13:51:45 +0800

    feat: [config] config ext

---
+++ src/main/kotlin/com/github/tsonglew/imageviewer/config/ApplicationSettingsConfigurable.kt
@@ -0,0 +1,22 @@
+package com.github.tsonglew.imageviewer.config
+
+import com.intellij.openapi.options.Configurable
+import javax.swing.JComponent
+
+class ApplicationSettingsConfigurable : Configurable {
+    override fun createComponent(): JComponent? {
+        TODO("Not yet implemented")
+    }
+
+    override fun isModified(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun apply() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getDisplayName(): String {
+        TODO("Not yet implemented")
+    }
+}
